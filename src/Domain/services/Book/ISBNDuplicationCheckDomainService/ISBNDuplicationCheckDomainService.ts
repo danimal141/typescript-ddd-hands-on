cddf9edfc9de02fb95b7b450308589e963beab52
danimal141 <hideaki.ishii1204@gmail.com>
2024-12-15 12:44:07 +0900

    Add ISBNDuplicationCheckDomainService test

--- src/Domain/services/Book/ISBNDuplicationCheckDomainService/ISBNDuplicationCheckDomainService.ts
+++ src/Domain/services/Book/ISBNDuplicationCheckDomainService/ISBNDuplicationCheckDomainService.ts
@@ -1,12 +1,16 @@
 import { BookId } from "Domain/models/Book/BookId/BookId";
+import { IBookRepository } from "Domain/models/Book/IBookRepository";
 
 // ドメインサービスを利用する判断軸:
 // 複数集約をまたいだ処理が必要な場合
 // DB ヘの問い合わせが必要な場合
 export class ISBNDuplicationCheckDomainService {
+  constructor(private bookRepository: IBookRepository) {}
+
   async execute(isbn: BookId): Promise<boolean> {
-    // 本来は、データベースに問い合わせて重複があるか確認する。この章では省略。
-    const isDuplicateISBN = false;
+    // データベースに問い合わせて重複があるか確認する
+    const duplicateISBNBook = await this.bookRepository.find(isbn);
+    const isDuplicateISBN = !!duplicateISBNBook;
 
     return isDuplicateISBN;
   }
